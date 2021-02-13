29e0dc7e34b390f59bf6d9fb85447e49d277116c
naoland <73999877+naoland@users.noreply.github.com>
2021-02-13 17:26:53 +0800

    refactor: update

--- app.go
+++ app.go
@@ -12,11 +12,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	r.GET("/last_price", func(c *gin.Context) {
 		price, err := fetchLastPrice()
 		if err != nil {
